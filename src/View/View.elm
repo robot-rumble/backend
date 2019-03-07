7ed8481af844c0272c078b2587ba398e4ec3be4e
Christian Sparks <csparks1919@hotmail.com>
2019-03-07 12:57:03 -0600

    Added view components

---
+++ src/View/View.elm
@@ -0,0 +1,9 @@
+module View exposing (..)
+
+import Html exposing (Html, textarea, div, p, text)
+import Html.Attributes exposing (class, classList, property, style)
+
+code = 
+  textarea [class "code"]
+
+info
