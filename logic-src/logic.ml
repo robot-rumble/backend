314d2a90e653f9c7b2c1c0f97ad237ebf503947d
Anton <antonoutkine@gmail.com>
2019-04-05 21:23:14 -0500

    Separate shared code into a library

---
+++ logic-src/logic.ml
@@ -0,0 +1,20 @@
+open Base
+
+let letters = String.to_array "abcdefghijklmnopqrstuvwxyz"
+
+let generate_id () =
+  String.init 5 ~f:(fun _ -> Array.random_element_exn letters)
+
+let create_unit ~type_ ~x ~y ~health ~team =
+  {Logic_t.type_; x; y; health; team; id= generate_id ()}
+
+let create_id_map unit_list =
+  List.map unit_list ~f:(fun unit_ -> (unit_.id, unit_))
+  |> Map.of_alist_exn (module String)
+
+let start run input =
+  let initial_state = {Logic_t.p1_code= [create_unit `Soldier 0 0 10]} in
+  let result = run input initial_state in
+  result
+
+let start () = "asf"
