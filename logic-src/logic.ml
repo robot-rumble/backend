c94dc930ae16dd2934adfa3c57719523dc67f38f
Anton <antonoutkine@gmail.com>
2019-04-10 10:58:23 -0500

    WIP

--- logic-src/logic.ml
+++ logic-src/logic.ml
@@ -24,26 +24,46 @@ let create_id_list unit_list =
 
 let create_team_list unit_list =
   let red, blue =
-    List.partition_map unit_list ~f:(fun unit ->
+    List.partition_map unit_list ~f:(fun (_, unit) ->
         match unit.team with Red -> `Fst unit.id | Blue -> `Snd unit.id )
   in
   [("red", red); ("blue", blue)]
 
 let radius' = 10
 let unit_num' = 6
+let other_team = function Red -> Blue | Blue -> Red
 
-let start run input =
-  let initial_map = create_circle_map radius' in
-  let initial_units =
-    List.init unit_num' ~f:(fun i ->
-        let x, y = random_loc initial_map
-        and team = if i % 2 = 0 then Red else Blue in
-        create_unit Soldier x y 10 team )
+let run_turn run turn units map custom_fields =
+  let team_list = create_team_list units in
+  let state = {turn= turn + 1; units; teams= team_list; map} in
+  let inputs =
+    List.map [Red; Blue] ~f:(fun team ->
+        { state
+        ; custom= Caml.List.assoc team custom_fields
+        ; friend= team
+        ; foe= other_team team } )
   in
-  List.iter initial_units ~f:(fun unit ->
-      initial_map.(unit.x).(unit.y) <- Unit unit.id );
-  let id_list = create_id_list initial_units
-  and team_list = create_team_list initial_units in
-  let state = {turn= 0; units= id_list; teams= team_list; map= initial_map} in
-  let result = run input.p1_code (Logic_j.string_of_state state) in
-  result
+  inputs |> List.map ~f:run |> Lwt.join
+  >|= fun (result : string) ->
+  let output_list = List.map result ~f:Logic_j.robot_output_of_string in
+  let output_list = List.zip_exn [Red; Blue] output_list in
+  List.iter output_list ~f:(fun (team, actions) ->
+      List.iter actions ~f:(fun (id, action) ->
+          match List.Assoc.find ~equal:String.( = ) units id with
+          | Some unit_ ->
+              if Poly.(unit_.team = team) then
+                failwith "Action ID belongs to opposing team."
+              else ()
+          | None -> failwith "Action ID does not exist." ) )
+
+let start run =
+  let map = create_circle_map radius' in
+  let units =
+    List.init unit_num' ~f:(fun i ->
+        let x, y = random_loc map and team = if i % 2 = 0 then Red else Blue in
+        let unit = create_unit Soldier x y 10 team in
+        (unit.id, unit) )
+  in
+  List.iter units ~f:(fun (_, unit) -> map.(unit.x).(unit.y) <- Unit unit.id);
+  let custom_fields = [(Red, ""); (Blue, "")] in
+  run_turn run 0 units map custom_fields
