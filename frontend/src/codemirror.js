dede0195ba2819c8d960baf4768d63fc07dc9f65
Anton <antonoutkine@gmail.com>
2019-05-23 16:36:47 -0500

    Add local saving

--- frontend/src/codemirror.js
+++ frontend/src/codemirror.js
@@ -24,7 +24,7 @@ customElements.define(
   class extends HTMLElement {
     constructor() {
       super()
-      this._value = sampleRobot
+      this._value = localStorage.getItem('code') || sampleRobot
     }
 
     // get value() {
