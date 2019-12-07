4e870b2da0edd2952363eb51f4666b914ed7276c
Anton <antonoutkine@gmail.com>
2019-12-06 20:15:26 -0600

    Display robot arena as a custom web component

--- src/frontend/src/app.js
+++ src/frontend/src/app.js
@@ -7,34 +7,36 @@ window.turnNum = 10
 window.language = 'python'
 window.runCount = 0
 
-window.onload = () => {
-  const app = Elm.Main.init({
-    node: document.getElementById('root'),
-    flags: {
-      totalTurns: window.turnNum,
-    },
-  })
+customElements.define('robot-arena', class extends HTMLElement {
+  connectedCallback () {
+    const app = Elm.Main.init({
+      node: this,
+      flags: {
+        totalTurns: window.turnNum,
+      },
+    })
 
-  const matchWorker = new window.Worker('/assets/dist/worker.js')
+    const matchWorker = new window.Worker('/assets/dist/worker.js')
 
-  app.ports.startEval.subscribe((code1) => {
-    window.runCount++
-    matchWorker.postMessage({ code1, code2: code1, turnNum: 20 })
-  })
+    app.ports.startEval.subscribe((code1) => {
+      window.runCount++
+      matchWorker.postMessage({ code1, code2: code1, turnNum: 20 })
+    })
 
-  matchWorker.onmessage = ({ data }) => {
-    if (data.type === 'error') {
-      console.log('Worker Error!')
-      console.error(data.data)
-      app.ports.getError.send(null)
-    } else {
-      if (data.type === 'getOutput') console.log(data.data)
-      app.ports[data.type].send(data.data)
+    matchWorker.onmessage = ({ data }) => {
+      if (data.type === 'error') {
+        console.log('Worker Error!')
+        console.error(data.data)
+        app.ports.getError.send(null)
+      } else {
+        if (data.type === 'getOutput') console.log(data.data)
+        app.ports[data.type].send(data.data)
+      }
     }
-  }
 
-  app.ports.reportDecodeError.subscribe((error) => {
-    console.log('Decode Error!')
-    console.error(error)
-  })
-}
+    app.ports.reportDecodeError.subscribe((error) => {
+      console.log('Decode Error!')
+      console.error(error)
+    })
+  }
+})
