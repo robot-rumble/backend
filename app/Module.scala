bb9dbc013bf0f7af3107c1c756b638dae5d4ce8c
Anton <antonoutkine@gmail.com>
2020-01-08 15:04:43 -0600

    Delete boilerplate components

--- app/Module.scala
+++ app/Module.scala
@@ -1,5 +1,3 @@
-import java.time.Clock
-
 import com.google.inject.AbstractModule
 import play.api.{Configuration, Environment}
 import services._
@@ -17,13 +15,6 @@ import services._
 
 class Module(environment: Environment, configuration: Configuration) extends AbstractModule {
   override def configure(): Unit = {
-    // Use the system clock as the default implementation of Clock
-    bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    // Ask Guice to create an instance of ApplicationTimer when the
-    // application starts.
-    bind(classOf[ApplicationTimer]).asEagerSingleton()
-    // Set AtomicCounter as the implementation for Counter.
-    bind(classOf[Counter]).to(classOf[AtomicCounter])
     bind(classOf[Db]).to(classOf[Postgres])
   }
 }
