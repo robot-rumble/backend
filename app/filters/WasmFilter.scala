8625ada1b54729a32f306cb403a199f4a359aea2
Anton <antonoutkine@gmail.com>
2020-01-08 15:04:21 -0600

    Fix WASM content-type with filter instead of controller

--- app/filters/WasmFilter.scala
+++ app/filters/WasmFilter.scala
@@ -5,14 +5,6 @@ import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
 
-/**
- * This is a simple filter that adds a header to all requests. It's
- * added to the application's list of filters by the
- * [[Filters]] class.
- *
- * @param ec This class is needed to execute code asynchronously.
- *           It is used below by the `map` method.
- */
 @Singleton
 class WasmFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFilter {
   override def apply(next: EssentialAction) = EssentialAction { request =>
