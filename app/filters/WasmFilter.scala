8f99823eb0b2baffbfef2ae550c0c13f4d759758
Anton <antonoutkine@gmail.com>
2020-03-30 11:38:30 -0500

    Fix custom WASM filter

--- app/filters/WasmFilter.scala
+++ app/filters/WasmFilter.scala
@@ -7,11 +7,11 @@ import scala.concurrent.ExecutionContext
 
 @Singleton
 class WasmFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFilter {
-  override def apply(next: EssentialAction) = EssentialAction { request =>
+  override def apply(next: EssentialAction): EssentialAction = EssentialAction { request =>
     next(request).map { result =>
-      if (request.headers.get("uri").contains(".wasm"))
-        result.withHeaders("ContentType" -> "application/wasm")
-      else result
+      if (request.headers.get("Raw-Request-URI").forall(_.contains(".wasm"))) {
+        result.as("application/wasm")
+      } else result
     }
   }
 }
