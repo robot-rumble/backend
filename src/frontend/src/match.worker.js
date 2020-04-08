6ce7729fe0936e2e9442b9ef50151254923138ad
Anton <antonoutkine@gmail.com>
2020-04-08 15:11:33 -0500

    Fix eslint warnings

--- src/frontend/src/match.worker.js
+++ src/frontend/src/match.worker.js
@@ -1,7 +1,3 @@
-import stdlib from './stdlib.raw.py'
-import zip from 'lodash/zip'
-import fromPairs from 'lodash/fromPairs'
-
 const logicPromise = import('logic')
 
 // to fix some weird bug, set the `Window` global to the worker global scope class
@@ -22,11 +18,11 @@ const errorToObj = (e, errorType) => {
       endch: e.endcol == null ? -1 : e.endcol,
     }
   }
-  if (e.constructor.name == "PyError") {
+  if (e.constructor.name === 'PyError') {
     // TODO: extract more info from this, display it nicely maybe
     message = e.toString()
     for (const tb of e.traceback) {
-      if (tb.filename == "<robot>") {
+      if (tb.filename === '<robot>') {
         errorLoc = {
           line: tb.lineno,
           ch: 0,
