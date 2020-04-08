fbd5008804dd63b6adfa63c573b97e34036c8cda
Anton <antonoutkine@gmail.com>
2020-04-08 13:57:19 -0500

    Merge evolutions and add robot_matches table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,14 +1,31 @@
--- Users schema
-
 -- !Ups
 
 CREATE TABLE users
 (
-    Id       SERIAL PRIMARY KEY,
+    id       SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE robots
+(
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(50) NOT NULL,
+    code    TEXT        NOT NULL,
+    user_id SERIAL      NOT NULL REFERENCES users (id)
+);
+
+CREATE TABLE robot_matches
+(
+    id            SERIAL PRIMARY KEY,
+    red_robot_id  SERIAL  NOT NULL REFERENCES robots (id),
+    blue_robot_id SERIAL  NOT NULL REFERENCES robots (id),
+    red_won       BOOLEAN NOT NULL,
+    data          TEXT    NOT NULL
+);
+
 -- !Downs
 
+DROP TABLE robot_matches;
+DROP TABLE robots;
 DROP TABLE users;
