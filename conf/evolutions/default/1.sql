dc9851a89e23b057d8434e5a519b812f6a6aebdd
Anton <antonoutkine@gmail.com>
2020-04-09 15:59:34 -0500

    Add even more fields to SQL migration in preparation for matchmaking

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,7 @@
 CREATE TABLE users
 (
     id       SERIAL PRIMARY KEY,
+    created  TIMESTAMP   NOT NULL DEFAULT NOW(),
     bio      TEXT,
     username VARCHAR(15) NOT NULL,
     password VARCHAR(50) NOT NULL
@@ -11,25 +12,35 @@ CREATE TABLE users
 CREATE TABLE robots
 (
     id          SERIAL PRIMARY KEY,
+    created     TIMESTAMP   NOT NULL DEFAULT NOW(),
     user_id     SERIAL      NOT NULL REFERENCES users (id),
     name        VARCHAR(15) NOT NULL,
     bio         TEXT,
-    open_source BOOL        NOT NULL,
+    open_source BOOL        NOT NULL DEFAULT TRUE,
+    automatch   BOOL        NOT NULL DEFAULT TRUE,
     code        TEXT        NOT NULL,
     rating      INT         NOT NULL
 );
 
-CREATE TABLE robot_matches
+CREATE TABLE matches
 (
-    id            SERIAL PRIMARY KEY,
-    red_robot_id  SERIAL  NOT NULL REFERENCES robots (id),
-    blue_robot_id SERIAL  NOT NULL REFERENCES robots (id),
-    red_won       BOOLEAN NOT NULL,
-    data JSON NOT NULL
+    id        SERIAL PRIMARY KEY,
+    created   TIMESTAMP NOT NULL DEFAULT NOW(),
+    r1_id     SERIAL    NOT NULL REFERENCES robots (id),
+    r2_id     SERIAL    NOT NULL REFERENCES robots (id),
+    ranked    BOOL      NOT NULL DEFAULT TRUE,
+    r1_won    BOOL      NOT NULL,
+    r1_rating INT       NOT NULL,
+    r2_rating INT       NOT NULL,
+    r1_time   REAL      NOT NULL,
+    r2_time   REAL      NOT NULL,
+    r1_logs   TEXT,
+    r2_logs   TEXT,
+    data      JSON      NOT NULL
 );
 
 -- !Downs
 
-DROP TABLE robot_matches;
+DROP TABLE matches;
 DROP TABLE robots;
 DROP TABLE users;
