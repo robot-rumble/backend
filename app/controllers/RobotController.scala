e0be6a59787dc0882de8005f29b37106af73a7a8
Anton <antonoutkine@gmail.com>
2020-04-18 11:14:35 -0500

    Check for robot edit permission in controller
    Replace conditional template rendering with controller auth check

--- app/controllers/RobotController.scala
+++ app/controllers/RobotController.scala
@@ -66,10 +66,10 @@ class RobotController @Inject()(cc: MessagesControllerComponents,
       }
   }
 
-  def edit(user: String, robot: String): Action[AnyContent] = Action {
+  def edit(user: String, robot: String): Action[AnyContent] = authAction(parse.anyContent) { authUser =>
     implicit request =>
       (for {
-        user <- usersRepo.find(user)
+        user <- usersRepo.find(user) if user == authUser
         robot <- repo.find(user, robot)
       } yield (user, robot)) match {
         case Some((user, robot)) =>
