93e7984886a25b771296a29474e51042af1dacda
Anton <antonoutkine@gmail.com>
2020-04-23 11:42:25 -0500

    Add robot publishing system
    Instead of using a robot's code as the single source of truth, create a `PublishedRobots` table which stores different published robot versions. A robot will only be put in the matchmaking queue once the user "publishes" it. The code stored inside of `Robots` is still the code that is loaded into The Garage, but the matchmaking/public facing code is the code of the most recent robot in `PublishedRobots`. Add a new `publish` view/controller.

---
+++ app/models/PublishedRobots.scala
@@ -0,0 +1,49 @@
+package models
+
+import java.time.{LocalDate}
+
+import javax.inject.Inject
+import services.Db
+
+object PublishedRobots {
+
+  private def createData(robot: Robots.Data): Data = {
+    Data(robot_id = robot.id, code = robot.dev_code)
+  }
+
+  case class Data(
+      id: Long = -1,
+      // TODO: make sure postgres sets this
+      created: LocalDate = LocalDate.MIN,
+      robot_id: Long,
+      code: String,
+  )
+
+  class Repo @Inject()(val db: Db, val usersRepo: Users.Repo) {
+
+    import db.ctx._
+
+    val schema: db.ctx.Quoted[db.ctx.EntityQuery[Data]] = quote(
+      querySchema[Data]("published_robots")
+    )
+
+    def create(robot: Robots.Data): Data = {
+      val data = createData(robot)
+      val (id, created) = run(
+        schema
+          .insert(lift(data))
+          .returningGenerated(row => (row.id, row.created))
+      )
+      data.copy(id = id, created = created)
+    }
+
+    def find(robot: Robots.Data): Option[Data] = {
+      run(
+        schema
+          .filter(_.robot_id == lift(robot.id))
+          .sortBy(_.created)(Ord.desc)
+      ).headOption
+    }
+  }
+
+}
