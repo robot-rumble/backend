a9867a888ae5da75cf4e23ba24e0b0c00866d9c9
Anton <antonoutkine@gmail.com>
2020-04-28 10:15:20 -0500

    WIP Create injectable AWS stream interface

--- app/services/MockQueue.scala
+++ app/services/MockQueue.scala
@@ -1,17 +1,17 @@
 package services
 
-import akka.NotUsed
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import javax.inject.Inject
-import services.MatchMaker.{MatchInput, MatchOutput, inputToOutput}
+import services.BattleQueue.{MatchInput, MatchOutput, inputToOutput}
 
 class MockQueue @Inject()(
     implicit materializer: Materializer
 ) extends BattleQueue {
-  def createStreams()
-    : (Sink[MatchInput, NotUsed], Source[MatchOutput, NotUsed]) = {
-    // https://discuss.lightbend.com/t/create-source-from-sink-and-vice-versa/605/4
+  val sink = Sink.onComplete(_ => ())
+  val source = Source.empty
+
+// https://discuss.lightbend.com/t/create-source-from-sink-and-vice-versa/605/4
 //    Source
 //      .asSubscriber[MatchOutput]
 //      .toMat(
@@ -23,7 +23,4 @@ class MockQueue @Inject()(
 //        case (sub, pub) => (Sink.fromSubscriber(sub), Source.fromPublisher(pub))
 //      }
 //      .run()
-
-    (Sink.onComplete(_ => ()), Source.empty)
-  }
 }
