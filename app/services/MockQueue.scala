500e5bf593ddc6f2c0fef9eb895a69259c5a0c2d
Anton <antonoutkine@gmail.com>
2020-04-29 18:40:20 -0500

    Randomly decide mock battle outcome

--- app/services/MockQueue.scala
+++ app/services/MockQueue.scala
@@ -8,13 +8,22 @@ import models.Battles.Winner
 import services.BattleQueue.{MatchInput, MatchOutput}
 
 import scala.concurrent.duration._
+import scala.util.Random
 
 class MockQueue @Inject()(
     implicit materializer: Materializer
 ) extends BattleQueue {
 
   def inputToOutput(input: MatchInput): MatchOutput = {
-    MatchOutput(input.r1Id, 0, input.r2Id, 0, Winner.Draw, false, "")
+    MatchOutput(
+      input.r1Id,
+      0,
+      input.r2Id,
+      0,
+      if (Random.nextBoolean()) Winner.R1 else Winner.R2,
+      false,
+      ""
+    )
   }
 
   // https://discuss.lightbend.com/t/create-source-from-sink-and-vice-versa/605/4
