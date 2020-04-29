650e5eff20e1c58e4dddb619fdf44d2ed22b916d
Anton <antonoutkine@gmail.com>
2020-04-29 16:57:25 -0500

    Create a mock akka queue that works. (the problem was that the receiver function errored)

--- app/services/MockQueue.scala
+++ app/services/MockQueue.scala
@@ -1,26 +1,33 @@
 package services
 
-import akka.stream.Materializer
+import akka.NotUsed
+import akka.stream.{Materializer, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import javax.inject.Inject
-import services.BattleQueue.{MatchInput, MatchOutput, inputToOutput}
+import models.Battles.Winner
+import services.BattleQueue.{MatchInput, MatchOutput}
+
+import scala.concurrent.duration._
 
 class MockQueue @Inject()(
     implicit materializer: Materializer
 ) extends BattleQueue {
-  val sink = Sink.onComplete(_ => ())
-  val source = Source.empty
 
-// https://discuss.lightbend.com/t/create-source-from-sink-and-vice-versa/605/4
-//    Source
-//      .asSubscriber[MatchOutput]
-//      .toMat(
-//        Flow[MatchInput]
-//          .map(inputToOutput)
-//          .to(Sink.asPublisher[MatchOutput](fanout = false))
-//      )(Keep.both)
-//      .mapMaterializedValue {
-//        case (sub, pub) => (Sink.fromSubscriber(sub), Source.fromPublisher(pub))
-//      }
-//      .run()
+  def inputToOutput(input: MatchInput): MatchOutput = {
+    MatchOutput(input.r1Id, 0, input.r2Id, 0, Winner.Draw, false, "")
+  }
+
+  // https://discuss.lightbend.com/t/create-source-from-sink-and-vice-versa/605/4
+  val in = Sink.asPublisher[MatchOutput](false)
+  val out = Source.asSubscriber[MatchOutput]
+
+  val (_sink, _source) = out
+    .toMat(in)(Keep.both)
+    .mapMaterializedValue {
+      case (sub, pub) => (Sink.fromSubscriber(sub), Source.fromPublisher(pub))
+    }
+    .run()
+
+  val sink = Flow[MatchInput].map(inputToOutput).to(_sink)
+  val source = _source
 }
