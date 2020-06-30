fd212cfad6e368a7b34e2c93f4d26dbad4f278bd
Anton <antonoutkine@gmail.com>
2020-06-30 18:41:57 -0500

    Add link to docs

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,17 +1,21 @@
 package controllers
 
 import javax.inject._
+import play.api.Configuration
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(
     cc: ControllerComponents,
-    assetsFinder: AssetsFinder
+    assetsFinder: AssetsFinder,
+    config: Configuration,
 ) extends AbstractController(cc) {
 
   def index = Action { implicit request =>
     Ok(views.html.index(assetsFinder))
   }
 
-  def rules = TODO
+  def rules = Action { implicit request =>
+    Redirect(config.get[String]("site.docsUrl"))
+  }
 }
