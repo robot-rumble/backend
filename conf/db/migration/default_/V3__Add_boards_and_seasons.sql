2572073bfc77d3b7b7286aa3156adb82f92d5862
Anton <antonoutkine@gmail.com>
2021-01-05 03:57:08 -0600

    Reorder third migration script and update new r_id field

--- conf/db/migration/default_/V3__Add_boards_and_seasons.sql
+++ conf/db/migration/default_/V3__Add_boards_and_seasons.sql
@@ -24,21 +24,35 @@ CREATE TABLE boards
     recurrent_battle_num INT         NOT NULL
 );
 
-ALTER TABLE published_robots
-    ADD COLUMN r_id BIGINT NOT NULL REFERENCES robots (id) DEFAULT 0;
-ALTER TABLE published_robots
-    ADD COLUMN board_id BIGINT NOT NULL REFERENCES boards (id) DEFAULT 0;
-ALTER TABLE robots
-    DROP COLUMN pr_id;
-ALTER TABLE robots
-    ADD COLUMN published BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE battles
-    ADD COLUMN board_id BIGINT NOT NULL REFERENCES boards (id) DEFAULT 0;
+INSERT INTO boards(name, publishing_enabled, matchmaking_enabled, publish_cooldown,
+                   publish_battle_num, battle_cooldown, recurrent_battle_num)
+VALUES ('Playground Board', FALSE, FALSE, 86400, 2, 86400, 1);
 
 ALTER TABLE published_robots
+    ADD COLUMN r_id BIGINT NOT NULL REFERENCES robots (id) DEFAULT 1;
+
+UPDATE
+    published_robots pr
+SET
+    r_id = r.id
+FROM robots r
+WHERE r.pr_id = pr.id;
+
+ALTER TABLE robots
+    DROP COLUMN pr_id;
+
+ALTER TABLE published_robots
+    ADD COLUMN board_id BIGINT NOT NULL REFERENCES boards (id) DEFAULT 1;
+ALTER TABLE published_robots
     ADD COLUMN rating INT NOT NULL DEFAULT 1000;
+
+ALTER TABLE robots
+    ADD COLUMN published BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE robots
     DROP COLUMN rating;
+
+ALTER TABLE battles
+    ADD COLUMN board_id BIGINT NOT NULL REFERENCES boards (id) DEFAULT 1;
 ALTER TABLE battles
     RENAME COLUMN r1_rating to pr1_rating;
 ALTER TABLE battles
@@ -47,3 +61,4 @@ ALTER TABLE battles
     RENAME COLUMN r1_rating_change to pr1_rating_change;
 ALTER TABLE battles
     RENAME COLUMN r2_rating_change to pr2_rating_change;
+
